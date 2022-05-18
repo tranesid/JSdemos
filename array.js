ace1508bdab239a9970ae620815c753d69e3adfb
Sidney <tranesidney@gmail.com>
2022-05-18 13:09:13 -0600

    notes

--- array.js
+++ array.js
@@ -39,7 +39,7 @@ for(let i = 0; i<=9; i+=2){
 // normal we like to loop through arrays
 
 console.log('show me things in the array')
-let nums =[10,20,30,40, -10, 100, 45]
+let nums =[10, 20, 30, 40, -10, 100, 45]
 
 for(let i = 0; i < nums.length; i++){
     // execute this block
@@ -80,4 +80,80 @@ timesByTwo(6)
 //or 
 timesByTwo(num)
 
+let people = ['George', 'Paul', 'Sally']
 
+//mutations (side effect)
+//add something to an array
+
+//when we get to react we won't add this way
+// no mutation.. in React
+
+//cloning below..
+// this method isn't mutation
+
+// let newClone = [...people,'frank']
+// // or
+// let newClone = [...people]
+// peopleClone.push('Frank')
+
+let people = ['George', 'Paul', 'Sally']
+
+// mutations (side effect)
+// add something to an array
+// people.push('Jill')
+
+// [x,x,x,x,x] =func=>[y,y,y,y,y]
+
+
+
+// when we get to react we are not going to want to add this
+// no mutation..
+
+// does not change people no side effect/mutation
+let newPeople = [...people,'frank']
+
+
+console.log(people)
+console.log(newPeople)
+
+const wrapInHeader = (str)=>{
+    return `<h1>${str}</h1>`
+}
+
+
+const header1 = wrapInHeader('YO')
+console.log(header1)
+
+// map return a new array (doesn't mutate the array it was called on)
+const peopleHeaders = people.map(wrapInHeader)
+console.log(peopleHeaders)
+console.log(people)
+
+
+const getCharArray = (str)=>{
+    return str.split('')
+}
+
+let chars = getCharArray('helllo')
+console.log(chars)
+
+let nameArrays = people.map(getCharArray)
+console.log(nameArrays)
+// [[][][]]
+nameArrays.forEach(nameArray=>{
+    console.log(nameArray.join('-'))
+    nameArray.forEach((letter)=>{
+        console.log(letter)
+    })
+})
+
+let x = {
+    d:[1,2,3,{t:{y:'YAY'}},4],
+    s:{q:[1,2,3,5,6,7,8,9,10]}
+}
+
+console.log(x.d[3].t.y)
+let greaterThan5 = x.s.q.filter((num)=> {
+    return num >=5
+})
+console.log(greaterThan5)
